c7e16afd67e83600880e4e770c42a25bb30ce85b
Tyler Johnson <tyler@csdconsultants.us>
2021-04-08 22:40:56 -0700

    added form from bootstrap. Filled with City name for form title and Explore for button text. Created state contructor and assigned city to an empty string. Form control value is set to the state.city. handleFormSubmit event listener takes onInput value of form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 class App extends React.Component {
-  render () {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      city: ''
+    }
+  }
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.city);
+  }
+
+  render() {
     return (
-      <h1>City Explorer</h1>
+      <>
+        <h1>City Explorer</h1>
+        <Form onSubmit={this.handleFormSubmit}>
+          <Form.Group controlId="city">
+            <Form.Label>City Name</Form.Label>
+            <Form.Control value={this.state.city} onInput={e => this.setState({ city: e.target.value })}></Form.Control>
+          </Form.Group>
+        </Form>
+        <Button variant="primary" type="submit">Explore!</Button>
+      </>
     )
   }
 }
